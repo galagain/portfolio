f37c31b1d8178f9cd2527d2b2c8e013bc534848f
Calvin GALAGAIN <galagain@calvin.lan>
2025-01-30 02:26:32 +0100

    feat: add resume, projects, and contact sections; enhance navigation functionality

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelectorAll(".navbar nav ul li a");
   const contentTitle = document.querySelector(".content-title h1");
+  const sections = document.querySelectorAll("section");
+
+  sections.forEach((section) => section.classList.add("hidden"));
+  document.querySelector(".about").classList.remove("hidden");
 
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault();
 
-      if (this.textContent.trim() === "About") {
-        contentTitle.textContent = "About Me";
-      } else {
-        contentTitle.textContent = this.textContent;
-      }
-      navLinks.forEach((nav) => nav.classList.remove("active"));
+      const sectionClass = this.textContent.trim().toLowerCase();
 
+      contentTitle.textContent =
+        this.textContent.trim() === "About" ? "About Me" : this.textContent;
+
+      sections.forEach((section) => section.classList.add("hidden"));
+
+      const activeSection = document.querySelector(`.${sectionClass}`);
+      if (activeSection) {
+        activeSection.classList.remove("hidden");
+      }
+
+      navLinks.forEach((nav) => nav.classList.remove("active"));
       this.classList.add("active");
     });
   });
